de4619e23085e4e12de150ba2b51af390415bd76
Kayli Pike <156618019+kaylipike@users.noreply.github.com>
2025-10-20 14:19:42 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p><a href='../final/index.php'>Site map</a></p>
+    <p><a href='https://knpike.w3.uvm.edu/cs1080/sitemap.php'>Site map</a></p>
     <p><a href='//knpike.w3.uvm.edu/cs1080/final'>Kayli Pike - Section A</a></p>
     <p>Background image source: <cite><a href="https://www.pinterest.com/pin/811914639046080955/">Pinterest</a></cite></p>
 </footer>
